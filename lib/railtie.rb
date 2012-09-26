5aa402531e46e26e927d8edb77e8cfb96fe24f93
Venkat Dinavahi <vendiddy@gmail.com>
2012-09-26 16:48:37 -0400

    bumped version and added railtie

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,7 +1,10 @@
+require 'compute'
+
 module Compute
   class Railtie < Rails::Railtie
     initializer 'compute.model_additions' do
       ActiveSupport.on_load :active_record do
+        include Compute
       end
     end
   end
